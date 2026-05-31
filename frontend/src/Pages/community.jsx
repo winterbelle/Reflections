351f683dd06060c18845ec1501a1a9fc687123ae
Santiago González <santiago.galvez@correounivalle.edu.co>
2026-05-31 11:09:49 -0400

    style: added style to the posts feed (work in progress)

--- frontend/src/Pages/community.jsx
+++ frontend/src/Pages/community.jsx
@@ -4,6 +4,8 @@ import React from "react";
 import PostFeed from "../components/Posts/PostFeed";
 import "../components/posts/PostFeed.css";
 import "../components/posts/PostPreview.css";
+import "../components/posts/Filter.css";
+import "./community.css";
 const Community = () => {
   return (
     <div className="community-page">
--- frontend/src/components/posts/PostFeed.jsx
+++ frontend/src/components/posts/PostFeed.jsx
@@ -4,7 +4,7 @@
 import React, { useEffect, useState } from "react";
 import PostCard from "./PostCard";
 import PostPreview from "./PostPreview";
-
+import Filter from "./Filter";
 const PostFeed = () => {
     const [posts, setPosts] = useState([]);
     useEffect(() => {
@@ -23,11 +23,23 @@ const PostFeed = () => {
     }, []);
 
     return (
-        <div className="post-feed">
-            {posts.map((post) => (
-                <PostPreview key={post.id} post={post} />
-            ))}
+        <>
+        <div className="post-feed-container">
+            <div className="post-feed-header">
+                <h2>Component to make a post goes here</h2>
+            </div>
+            <div className="post-feed-main-content">
+                <div className="post-feed-filters">
+                    <Filter />
+                </div>
+                <div className="post-feed-posts">
+                    {posts.map((post) => (
+                        <PostPreview key={post.id} post={post} />
+                    ))}
+                </div>
+            </div>
         </div>
+        </>
     );
 };
 
